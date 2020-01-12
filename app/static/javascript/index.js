4c3b2d63e7938c253d1773a275c207e2857abe64
AedenGB <aeden.gb@gmail.com>
2020-01-12 00:42:03 -0800

    some initial formatting changes, mostly to popups, themes, and title boxes

--- app/static/javascript/index.js
+++ app/static/javascript/index.js
@@ -5,30 +5,8 @@ $(document).ready(function(){
       }
     }
     var setColors = function(){
-      if(window.location.hash == "#light"){
-        $('#theme-button').text("Dark Theme");
-        $('#menu-bar').css('background',"white");
-        $('html').css('background',"white");
-        $('.text').css('color',"black");
-        $('.links').css('color',"black");
-        $('#title').css("color","blue");
-        $('.member-profile').css("box-shadow","4px 4px 3px #999");
-        $('#contact-popup').css("background","rgb(254,254,254)");
-        $('#contact-popup').css("box-shadow","6px 6px 4px #999");
-        $('#all-awards-scrolling-div').css("background","rgb(254,254,254)");
-        $('#all-awards-scrolling-div').css("box-shadow","6px 6px 4px #999");
-        $('#all-awards-popup').css("width","815px");
-        $('.member-profile').css("background","lightGrey");
-        $('.awards').css("box-shadow","6px 6px 4px #999");
-        $('.awards').css("background","lightGrey");
-        if($(window).scrollTop()>15){
-          $('#menu-bar').css('box-shadow',"0 2px 2px #999");
-        }
-      }
-      else{
-        window.location.hash = "#dark"
+      if(window.location.hash == "#dark"){
         $('.awards').css("background","grey");
-        $('#theme-button').text("Light Theme");
         $('#menu-bar').css('background',"#111");
         $('html').css('background',"#111");
         $('.text').css('color',"white");
@@ -47,25 +25,34 @@ $(document).ready(function(){
           $('#menu-bar').css('box-shadow',"0 2px 6px #666");
         }
       }
+      else{
+        $('#menu-bar').css('background',"white");
+        $('html').css('background',"white");
+        $('.text').css('color',"black");
+        $('.links').css('color',"black");
+        $('#title').css("color","blue");
+        $('.member-profile').css("box-shadow","4px 4px 3px #999");
+        $('#contact-popup').css("background","rgb(254,254,254)");
+        $('#contact-popup').css("box-shadow","3px 3px 4px #999");
+        $('#all-awards-scrolling-div').css("background","rgb(254,254,254)");
+        $('#all-awards-scrolling-div').css("box-shadow","6px 6px 4px #999");
+        $('#all-awards-popup').css("width","815px");
+        $('.member-profile').css("background","lightGrey");
+        $('.awards').css("box-shadow","6px 6px 4px #999");
+        $('.awards').css("background","lightGrey");
+        if($(window).scrollTop()>15){
+          $('#menu-bar').css('box-shadow',"0 2px 2px #999");
+        }
+      }
     }
     setColors();
     $('html').hide();
     $('html').fadeIn(1000);
-    $('#theme-button').on( "click", function(){
-      if(window.location.hash == "#dark"){
-        window.location.hash = "#light";
-      }
-      else{
-        window.location.hash = "#dark"
-      }
-      setColors();
-    });
 
     $("#contact-us").on( "click", function(){//if contact us button pressed, open popup
         $("#contact-popup").css("visibility","visible");
         $("#grey").css("visibility","visible");
         $('html').bind('mousewheel touchmove', lockScroll);
-        $("body").css("opacity","0.5");
         return;
     });
 
@@ -73,20 +60,17 @@ $(document).ready(function(){
         $("#all-awards-popup").css("visibility","visible");
         $("#grey").css("visibility","visible");
         $('body').bind('mousewheel touchmove', lockScroll);
-        $("body").css("opacity","0.5");
         return;
     });
 
     $("#grey").on( "click", function(){//if grey box clicked while popup open, close popup
       if($("#contact-popup").css("visibility")==="visible"){
-        $("body").css("opacity","1");
         $('html').unbind('mousewheel touchmove', lockScroll);
         $("#contact-popup").css("visibility","collapse");
         $("#grey").css("visibility","collapse");
         return;
       }
       if($("#all-awards-popup").css("visibility")==="visible"){
-        $("body").css("opacity","1");
         $('body').unbind('mousewheel touchmove', lockScroll);
         $("#all-awards-popup").css("visibility","collapse");
         $("#grey").css("visibility","collapse");
