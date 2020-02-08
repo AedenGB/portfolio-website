932cb07eae8e23934c00a401b5a43821e1e88e35
AedenGB <aeden.gb@gmail.com>
2020-02-07 21:43:03 -0800

    All working with scrollbar and resizing

--- app/static/javascript/index.js
+++ app/static/javascript/index.js
@@ -1,91 +1,91 @@
 $(document).ready(function(){
+  var underline_position = '#Homepage';
+
   $('html').hide();
   $('html').fadeIn(1000);
-  $('#menu-bar').css("background", "rgba(256,256,256,0)");
-
   $.localScroll({
      target:'body',
      duration: 900
   });
 
-  function limit_element_size_to(element, max_width, padding, parent) {
-    var parent_width = $(parent).width();
-    if(parent_width<max_width+(padding*2)){
-      $(element).css("width",parent_width-(padding*2));
-    }
-    else{
-      $(element).css("width",max_width);
-    }
-  }
-
   function adjust_for_window_size() {
-    var viewport_width = $(window).width();
-    var viewport_height = $(window).height();
-
-    $("html").css("font-size", viewport_width*0.0022+14);//set main font size, mostly for body-content
-    $(".heading").css("font-size", viewport_width*0.0045+15);//set heading font size
-    $("#home-icon").css("height", viewport_width*0.0045+15);
-    $(".subheading").css("font-size", viewport_width*0.004+14);//set subheading font size
-
     var menu_height = $("#menu-bar").height();//based on height of elements in menu bar, adjust scroll characteristics
     $(".anchor").css("top",-menu_height-10);
-
-    limit_element_size_to(".width-controlled", 700, 10, window);//all body text width controlled elements
-    limit_element_size_to("#contact-popup", 400, 10, window);//contact popup width limit
-
-    $("#resume").css("height", $("#resume").width()*11/8.5);//set aspect ratio for pdf
   };
+  function menu_scroll_underline() {
+    var top_of_screen = $(window).scrollTop();
+    var menu_height = 2*$("#menu-bar").height();
+    var homepage = $("#Homepage-Section").offset().top;
+    var employment = $("#Employment-Section").offset().top;
+    var projects = $("#Projects-Section").offset().top;
+    var education = $("#Education-Section").offset().top;
+    var resume = $("#Resume-Section").offset().top;
 
-  adjust_for_window_size();
 
-  $(window).resize(adjust_for_window_size);
+    if($(".menu-link[href='#Homepage']:hover").length != 0){
+      underline_position = "#Homepage";
+    }else if($(".menu-link[href='#Employment']:hover").length != 0){
+      underline_position = "#Employment";
+    }else if($(".menu-link[href='#Projects']:hover").length != 0){
+      underline_position = "#Projects";
+    }else if($(".menu-link[href='#Education']:hover").length != 0){
+      underline_position = "#Education";
+    }else if($(".menu-link[href='#Resume']:hover").length != 0){
+      underline_position = "#Resume";
+    }else if($(".menu-link[href='#Contact']:hover").length != 0){
+      underline_position = "#Contact";
+    }
+    else if($("#contact-popup").css("max-width") != '0px'){
+      underline_position = "#Contact";
+    }else if (top_of_screen > resume-menu_height){
+      underline_position = "#Resume";
+    }else if (top_of_screen > education-menu_height){
+      underline_position = "#Education";
+    }else if (top_of_screen > projects-menu_height){
+      underline_position = "#Projects";
+    }else if (top_of_screen > employment-menu_height){
+      underline_position = "#Employment";
+    }else{
+      underline_position = "#Homepage";
+    }
+  }
+  function position_underline(position){
+    var top_of_screen = $(window).scrollTop();
+    $("#menu-underline").css('top', $(".menu-link[href='"+position+"']").offset().top-top_of_screen+$(".menu-link[href='"+position+"']").outerHeight());
+    $("#menu-underline").css('margin-left', $(".menu-link[href='"+position+"']").offset().left);
+    $("#menu-underline").css('width', $(".menu-link[href='"+position+"']").css('width'));
+  }
+  function menu_background() {
+    var top_of_screen = $(window).scrollTop();
+    var homepage_bottom = $("#Homepage-Section").offset().top + $("#Homepage-Section").outerHeight();
 
-  $("#six").on( "click", function(){//if contact us button pressed, open popup
-      $("#contact-popup").css("max-width","400");
-      $("#grey").css("visibility","visible");
-      $("#grey").css("opacity","0.6");
-      return;
+    if (top_of_screen < homepage_bottom){
+      $('#menu-bar').css('background',"rgba(256,256,256,0.0)");
+    } else {
+      $('#menu-bar').css('background',"rgba(256,256,256,1)");
+    }
+  }
+  function all_actions() {
+    menu_background();
+    menu_scroll_underline();
+    position_underline(underline_position);
+    adjust_for_window_size();
+  }
+  all_actions();
+
+  $(window).on('mousemove scroll resize', all_actions);
+
+  $("#contact-button").on( "click", function(){//if contact us button pressed, open popup
+    $("#contact-popup").css("max-width","400");
+    $("#grey").css("visibility","visible");
+    $("#grey").css("opacity","0.6");
   });
-
   $(".popup-closer").on( "click", function(){//if grey box clicked while popup open, close popup
     $("#contact-popup").css("max-width","0");
     $("#grey").css("visibility","collapse");
     $("#grey").css("opacity","0");
-  });
 
-  $(window).scroll(function() {
-    var menu_height = $("#menu-bar").height();
-    var bottom_of_element = $("#Homepage-Section").offset().top + $("#Homepage-Section").outerHeight();
-    var top_of_screen = $(window).scrollTop();
-
-    if (top_of_screen < bottom_of_element-menu_height*6){
-      $('#menu-bar, .dropdown-container').css('box-shadow',"0 0");
-      $('#menu-bar, .dropdown-container').css('background',"rgba(256,256,256,0.0)");
-    } else {
-      $('#menu-bar, .dropdown-container').css('transition',"0.2s");
-      $('#menu-bar, .dropdown-container').css('box-shadow',"0 2px 6px #666");
-      $('#menu-bar, .dropdown-container').css('background',"rgba(256,256,256,1)");
-    }
-
-    var employment = $("#Employment-Section").offset().top + $("#Employment-Section").outerHeight();
-    var projects = $("#Projects-Section").offset().top + $("#Projects-Section").outerHeight();
-    var education = $("#Education-Section").offset().top + $("#Education-Section").outerHeight();
-    var resume = $("#Resume-Section").offset().top + $("#Resume-Section").outerHeight();
-
-
-
-    if (top_of_screen < employment-menu_height){
-      $(this).toggleClass('underlined');
-      console.log("4");
-    }else if (top_of_screen < projects-menu_height){
-      console.log("3");
-    }else if (top_of_screen < education-menu_height){
-      console.log("2");
-    }else if (top_of_screen < resume-menu_height){
-      console.log("1");
-    }else {
-      console.log("0");
-    }
-
+    all_actions();
+    console.log(underline_position);
   });
 });
