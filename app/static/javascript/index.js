a32dca71251ab91c4d092a21d7d3374c1e45a65b
AedenGB <aeden.gb@gmail.com>
2020-02-12 12:42:29 -0800

    added contact buttons

--- app/static/javascript/index.js
+++ app/static/javascript/index.js
@@ -15,11 +15,12 @@ $(document).ready(function(){
   function menu_scroll_underline() {
     var top_of_screen = $(window).scrollTop();
     var menu_height = 2*$("#menu-bar").height();
+
     var homepage = $("#Homepage-Section").offset().top;
+    var education = $("#Education-Section").offset().top;
+    var skills = $("#Skills-Section").offset().top;
     var employment = $("#Employment-Section").offset().top;
     var projects = $("#Projects-Section").offset().top;
-    var education = $("#Education-Section").offset().top;
-    var resume = $("#Resume-Section").offset().top;
     var contact = $("#Contact-Section").offset().top;
     var window_height = $(window).height();
 
@@ -27,27 +28,27 @@ $(document).ready(function(){
     console.log(top_of_screen);
     if($(".menu-link[href='#Homepage']:hover").length != 0){
       underline_position = "#Homepage";
+    } else if($(".menu-link[href='#Education']:hover").length != 0){
+        underline_position = "#Education";
+    } else if($(".menu-link[href='#Skills']:hover").length != 0){
+          underline_position = "#Skills";
     }else if($(".menu-link[href='#Employment']:hover").length != 0){
       underline_position = "#Employment";
     }else if($(".menu-link[href='#Projects']:hover").length != 0){
       underline_position = "#Projects";
-    }else if($(".menu-link[href='#Education']:hover").length != 0){
-      underline_position = "#Education";
-    }else if($(".menu-link[href='#Resume']:hover").length != 0){
-      underline_position = "#Resume";
     }else if($(".menu-link[href='#Contact']:hover").length != 0){
       underline_position = "#Contact";
     }
     else if (top_of_screen > contact-window_height){
       underline_position = "#Contact";
-    }else if (top_of_screen > resume-menu_height){
-      underline_position = "#Resume";
-    }else if (top_of_screen > education-menu_height){
-      underline_position = "#Education";
     }else if (top_of_screen > projects-menu_height){
       underline_position = "#Projects";
     }else if (top_of_screen > employment-menu_height){
       underline_position = "#Employment";
+    }else if (top_of_screen > skills-menu_height){
+      underline_position = "#Skills";
+    }else if (top_of_screen > education-menu_height){
+      underline_position = "#Education";
     }else{
       underline_position = "#Homepage";
     }
